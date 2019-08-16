75e30ccf3083fce928888cc591ad5f67751c65d2
GitHpotato <hengxipeng@qq.com>
2019-08-16 20:58:42 +0800

    push

--- Tangshi/axios/info/info.js
+++ Tangshi/axios/info/info.js
@@ -5,7 +5,7 @@
         cursoropacitymax: 0.5, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0 
         touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备 
         cursorwidth: "6px", //像素光标的宽度 
-        cursorborder: "0", //     游标边框css定义 
+        cursorborder: "0", //游标边框css定义 
         cursorborderradius: "3px",//以像素为光标边界半径 
         autohidemode: true //是否隐藏滚动条 
     });
