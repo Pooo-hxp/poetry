ce4bada5b5733cd290c88a635d18d2f12e0ea31c
GitHpotato <hengxipeng@qq.com>
2019-08-28 23:05:55 +0800

    完善留言板功能

--- Tangshi/hxp.php
+++ Tangshi/hxp.php
@@ -1,5 +1,9 @@
 <?php
-	// $username=$_POST['user'];
+ header('Content-Type: text/html;charset=utf-8');
+ header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+ header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
+ header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
+ header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin');
 	$success=array('msg'=>'ok');
 	$con=mysqli_connect('localhost','login','hengxipeng123','login');
 	if($con){
