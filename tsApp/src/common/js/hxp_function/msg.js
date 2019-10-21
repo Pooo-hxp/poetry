769fd7998cf225754af94db4d8cdb50f35446e39
GitHpotato <hengxipeng@qq.com>
2019-10-21 17:59:59 +0800

    增加PHP脚本

--- tsApp/src/common/js/hxp_function/msg.js
+++ tsApp/src/common/js/hxp_function/msg.js
@@ -1,3 +1,34 @@
 (function(){
-
+    var vm=new Vue({
+        el:'.app',
+        data:{},
+        methods:{
+            
+        },
+        components:{
+            hxpmsg:{
+                template:'#msg',
+                data:function() {
+                    return {
+                        list:null
+                    }
+                },
+                mounted() {
+                    axios
+                    .get('https://www.xipengheng.cn/AAA/test.php')
+                    .then(res=>{
+                        var newlist=res.data.message;
+                        this.list=newlist;
+                        console.log(this.list);
+                    })
+                },
+                methods: {
+                    tt:function(){
+                        alert();
+                    }
+                },
+            }
+        }
+    })
+    
 })()
