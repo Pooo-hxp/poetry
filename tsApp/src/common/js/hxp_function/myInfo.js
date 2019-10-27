4c1ea5bb7192b728687af8079e9b8e274665acfb
GitHpotato <hengxipeng@qq.com>
2019-10-27 17:06:53 +0800

    增加修改个人信息功能

--- tsApp/src/common/js/hxp_function/myInfo.js
+++ tsApp/src/common/js/hxp_function/myInfo.js
@@ -12,6 +12,52 @@
     // if( confirm('要跳转吗？')){
     //     alert("跳转了");
     // }else{
-
     // }
+    var vm=new Vue({
+        el:'#app',
+        data:{
+            table:false,/*弹窗是否显示*/
+            updateInfo:false,/*修改个人信息*/
+            login:true,/*注册、登录*/
+            UserName:null,
+            PassWord:null,
+            UserGender:null,
+            UserSayHi:null,
+        },
+        methods:{
+            loginfun:function(){
+                this.login=true;
+                this.updateInfo=false;
+            }, 
+            login_axios:function(){
+                var formdata=new FormData();
+                formdata.append('UserName',this.UserName);
+                formdata.append('PassWord',this.PassWord);
+                axios
+                .post('',formdata)
+                .then(res=>{
+                    console.log(res);
+                })
+            },
+            updatefun:function(){
+                this.updateInfo=true;
+                this.login=false;
+            },
+            close:function(){
+                this.table=false;
+            },
+            setting:function(){
+                this.table=true;
+                // var formdata=new FormData();
+                // formdata.append('UserName','guofeng');
+                // axios 
+                // .post('https://www.xipengheng.cn/AAA/myInfo.php',formdata)
+                // .then(res=>{
+                //     this.UserName=res.data.message[0].UserName;
+                //     this.UserGender=res.data.message[0].UserGender;
+                //     this.UserSayHi=res.data.message[0].UserSayHi;
+                // })
+            }
+        },
+    })
 })();
