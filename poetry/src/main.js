dfd7fa63cf9f1f794a3408d5e8b7f8dce7e26936
GitHpotato <hengxipeng@qq.com>
2019-11-18 19:22:35 +0800

    npm安装axios

--- poetry/src/main.js
+++ poetry/src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 import router from './router'
 import './assets/js/clickme'/*引入公共方法*/
 import './assets/css/share.css' /*引入公共样式*/
 
-Vue.config.productionTip = false
+Vue.prototype.$axios = axios //把对象挂载在vue中
+Vue.config.productionTip = false//不起用生产环境下的配置
 
 /* eslint-disable no-new */
 new Vue({
