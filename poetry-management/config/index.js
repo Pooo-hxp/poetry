0f0dc46994fdbdd875f5e7cdc7c82f0e5825a0eb
GitHpotato <hengxipeng@qq.com>
2019-12-07 16:23:36 +0800

    更改配置

--- poetry-management/config/index.js
+++ poetry-management/config/index.js
@@ -15,7 +15,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,//更改后，启动项目后自动打开浏览器emm
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
