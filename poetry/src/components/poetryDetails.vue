9712ccec5398b41b3c2af188716fe169688209e3
GitHpotato <hengxipeng@qq.com>
2020-02-03 17:21:14 +0800

    分割诗词，格式化展示

--- poetry/src/components/poetryDetails.vue
+++ poetry/src/components/poetryDetails.vue
@@ -3,47 +3,36 @@
     <div class="container">
       <div class="col-md-3 hxp-li-">
         <ul class="text-left hxp-li-margin">
-          <li>
-                <img src="../assets/images/photo/foo.jpg" alt="图片地址出错.." width="100px" height="100px" />
-          </li>
-          <li>
+          <img src="../assets/images/photo/foo.jpg" alt="图片地址出错.." width="100px" height="100px" />
+          <li class="text-center">
             <!-- 图标替换 -->
-              <img src="../assets/images/icon/内容.png"  style="width:60px;height:60px" />
+            <img src="../assets/images/icon/喜欢.png" style="width:40px;height:40px" />
+            收藏本诗
+          </li>
+          <li class="text-center">
+            <!-- 图标替换 -->
+            <img src="../assets/images/icon/内容.png" style="width:40px;height:40px" />
             诗词内容
           </li>
-          <li>
+          <div class="col-md-4">
+            <div class="row-fluid text-center">
+              <h3 class="text-success">《{{details[0].title}}》</h3>
+              <h4 class="text-right">--{{details[0].author}}</h4>
+              <p v-for="item in poetryinfo">{{item}}</p>
+            </div>
+          </div>
+          <li class="text-center">
             <!-- <svg class="icon" style="font-size: 25px;">
               <use xlink:href="#icon-rili" />
-            </svg> -->
-              <img src="../assets/images/icon/题型解析.png" style="width:60px;height:60px"  />
+            </svg>-->
+            <img src="../assets/images/icon/题型解析.png" style="width:40px;height:40px" />
             作品解析
           </li>
-          <li>
+          <div class="col-md-5">{{details[0].intro}}</div>
+          <li class="text-center">
             <a href="mailto:hengxipeng@163.com">联系我</a>
           </li>
         </ul>
-      </div>
-      <div class="col-md-4">
-        <h2 class="hxp-fontfamily">
-          <!-- <svg class="icon" style="font-size: 30px;">
-            <use xlink:href="#icon-BOOKMARKINGSERVICES" />
-          </svg> -->
-          诗词内容
-        </h2>
-        <!-- /*lead*/ -->
-        <div class="container-fluid hxp-size-">
-          <div class="app1">
-          </div>
-        </div>
-      </div>
-      <div class="col-md-5">
-        <h2 class="hxp-fontfamily">
-          诗词解析
-        </h2>
-        <div class="container-fluid hxp-size-">
-          <div class="app2">
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -53,45 +42,48 @@ export default {
   name: "poetryDetails",
   data() {
     return {
-      details: ""
+      details: "",
+      poetryinfo:''
     };
   },
   methods: {},
   mounted() {
     /**
      * 获取所点击的诗词详情
+     * 并分割诗词内容友好化展示
      */
     this.details = JSON.parse(sessionStorage.getItem("par"));
-    console.log(this.details);
+    this.poetryinfo=this.details[0].content.split('。')
+    console.log( this.poetryinfo);
   }
 };
 </script>
 <style  scoped>
 * {
-    padding: 0px;
-    margin: 0px;
-    font-size: 16px;
+  padding: 0px;
+  margin: 0px;
+  font-size: 16px;
 }
 
-.header{
-    padding: 1em;
-    border-bottom: 2px solid rgba(243, 237, 237,0.5);
+.header {
+  padding: 1em;
+  border-bottom: 2px solid rgba(243, 237, 237, 0.5);
 }
 
-ul li{
-    list-style: none;
-    border-bottom: 1px solid rgba(233, 175, 175, 0.8);
- }
+ul li {
+  list-style: none;
+  border-bottom: 1px solid rgba(233, 175, 175, 0.8);
+}
 
-.hxp-li-margin li{
-    line-height:3em;
-    width: 10em;
+.hxp-li-margin li {
+  line-height: 3em;
+  width: 10em;
 }
-.hxp-fontfamily{
-    font-family: STXingkai;
+.hxp-fontfamily {
+  font-family: STXingkai;
 }
-.hxp-size-{
-    height: 20em;
-    overflow-x: hidden;
+.hxp-size- {
+  /* height: 20em; */
+  overflow-x: hidden;
 }
 </style>
