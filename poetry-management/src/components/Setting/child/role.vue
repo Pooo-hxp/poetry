495e075601e0f1c5e4773981d71487492fb3003a
GitHpotato <hengxipeng@qq.com>
2020-02-25 18:47:08 +0800

    添加表头

--- poetry-management/src/components/Setting/child/role.vue
+++ poetry-management/src/components/Setting/child/role.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <H4>这里是角色管理组件</H4>
-    </div>
+  <div>
+    <H4>这里是角色管理组件</H4>
+    <table>
+      <thead>
+        <th>表头1</th>
+        <th>表头2</th>
+        <th>表头3</th>
+        <th>表头4</th>
+      </thead>
+    </table>
+  </div>
 </template>
 <script>
 export default {
-    name:'role',
-    data(){
-    },
-}
+  name: "role",
+  data() {}
+};
 </script>
 <style scoped>
-
 </style>
--- poetry-management/src/components/Setting/child/task.vue
+++ poetry-management/src/components/Setting/child/task.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
-        <h4>这是任务组件</h4>
-    </div>
+  <div>
+    <h4>这是任务组件</h4>
+    <table>
+      <thead>
+        <th>表头1</th>
+        <th>表头2</th>
+        <th>表头3</th>
+        <th>表头4</th>
+      </thead>
+    </table>
+  </div>
 </template>
 <script>
 export default {
-    name:'task',
-    data(){
-
-    }
-}
+  name: "task",
+  data() {}
+};
 </script>
 <style scoped>
-
 </style>
