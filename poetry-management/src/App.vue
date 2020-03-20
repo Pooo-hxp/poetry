5098262e0e274d9f49fef0852147c5da6a4d1420
GitHpotato <hengxipeng@qq.com>
2020-03-20 22:10:30 +0800

    去除无用提示信息

--- poetry-management/src/App.vue
+++ poetry-management/src/App.vue
@@ -5,22 +5,24 @@
 </template>
 
 <script>
-import Index from '@/components/Index'
+import Index from "@/components/Index";
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     Index
   }
-}
+};
 </script>
 
 <style>
-html,body,#app{
-width:100%;
-height:100%;
-}
-*{
-  padding: 0;
-  margin: 0;
-}
+html,
+body,
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+  * {
+    padding: 0;
+    margin: 0;
+  }
 </style>
--- poetry-management/src/main.js
+++ poetry-management/src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 /**
  * 入口文件
  * create from 20191207 of HXP
